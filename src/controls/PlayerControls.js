5c43c9c35d8f4b8a36f8b05268278aeb4ea351b1
kanawattown-a11y <kanawattown@gmail.com>
2025-08-25 11:22:50 +0400

    Update PlayerControls.js

--- src/controls/PlayerControls.js
+++ src/controls/PlayerControls.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
 export class PlayerControls {
